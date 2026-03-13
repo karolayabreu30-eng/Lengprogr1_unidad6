0544ccfe89ce98cc81df9099d434f311e7e1e774
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:32:48 -0400

    tarea2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿var productos = new Dictionary<string, double>
+{
+    { "Café", 2.50 },
+    { "Pan", 1.00 },
+    { "Leche", 1.20 }
+};
+
+Console.WriteLine($"El precio del Café es: ${productos["Café"]}");
