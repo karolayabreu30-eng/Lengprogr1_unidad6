3146eca1867348b8eee4b2de524de4799425ac4a
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:41:45 -0400

    tarea6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,6 @@
+﻿var colores = new Dictionary<string, string> { { "R", "Rojo" }, { "V", "Verde" }, { "A", "Azul" }, { "N", "Negro" }, { "B", "Blanco" } };
+
+foreach (var valor in colores.Values)
+{
+    Console.WriteLine($"Color: {valor}");
+}
