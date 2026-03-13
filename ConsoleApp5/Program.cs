cb8e8425445a563c25b97805960ebd69126afa3d
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:39:26 -0400

    Tarea5

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,6 @@
+﻿var frutas = new Dictionary<int, string> { { 1, "Manzana" }, { 2, "Pera" }, { 3, "Uva" }, { 4, "Mango" }, { 5, "Piña" } };
+
+foreach (var clave in frutas.Keys)
+{
+    Console.WriteLine($"ID: {clave}");
+}
