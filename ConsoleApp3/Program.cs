8a6efa3fd8503bb35d82c0879148cc047dfe83e5
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:34:43 -0400

    tarea3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿var estudiantes = new Dictionary<string, int>
+{
+    { "Ana", 85 },
+    { "Luis", 90 },
+    { "Marta", 78 }
+};
+
+estudiantes["Marta"] = 95; // Modificación
+Console.WriteLine($"Nueva nota de Marta: {estudiantes["Marta"]}");
