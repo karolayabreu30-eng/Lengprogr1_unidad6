1ff151fec0bf6bd6576da5ded5926ab9633ce8b2
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:36:43 -0400

    Tarea4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,9 @@
+﻿var agenda = new Dictionary<string, string>
+{
+    { "Trabajo", "555-0101" },
+    { "Casa", "555-0202" },
+    { "Celular", "555-0303" }
+};
+
+agenda.Remove("Casa");
+Console.WriteLine($"Elementos restantes: {agenda.Count}");
