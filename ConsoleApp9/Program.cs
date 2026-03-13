09b60ef84342550cd4a7d3e0730271c45b50d902
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:49:17 -0400

    tarea9

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿var gastos = new Dictionary<string, double>
+{
+    { "Internet", 40.0 },
+    { "Luz", 65.5 },
+    { "Agua", 20.0 },
+    { "Gas", 30.0 }
+};
+
+double total = 0;
+foreach (var monto in gastos.Values)
+{
+    total += monto;
+}
+
+Console.WriteLine($"La suma total de gastos es: ${total}");
