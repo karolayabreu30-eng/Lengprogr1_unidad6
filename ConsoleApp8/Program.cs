38c271cdeea8b7b1d4005989651007b943a7efce
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:46:02 -0400

    Tarea8

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,6 @@
+﻿var ciudades = new Dictionary<string, int> { { "Londres", 2000 }, { "París", 2024 }, { "Roma", 1998 } };
+
+foreach (KeyValuePair<string, int> par in ciudades)
+{
+    Console.WriteLine($"Ciudad: {par.Key}, Año de visita: {par.Value}");
+}
