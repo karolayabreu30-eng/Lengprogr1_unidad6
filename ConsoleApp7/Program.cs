aa5141fd2b82f3b471cb9b59fe154e86d78c6680
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-13 01:43:54 -0400

    tarea7

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,7 @@
+﻿var stock = new Dictionary<string, int> { { "Laptop", 5 }, { "Mouse", 10 }, { "Teclado", 7 } };
+
+string buscar = "Mouse";
+if (stock.ContainsKey(buscar))
+{
+    Console.WriteLine($"Sí, tenemos {buscar} en inventario.");
+}
